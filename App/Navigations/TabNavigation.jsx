c69fa54c521ff12888894a2a153ea5095910a9f1
Vinicius Lopes <viniloppes2000@gmail.com>
2024-01-08 23:16:32 -0300

    up

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -6,20 +6,36 @@ import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { FontAwesome } from '@expo/vector-icons';
 const Tab = createMaterialBottomTabNavigator();
 export default function TabNavigation() {
     return (
         <Tab.Navigator  >
-            <Tab.Screen name="home" component={HomeScreen}
+            <Tab.Screen name="Inicio" component={HomeScreen}
                 options={{
-                    tabBarLabel: 'Home',
+                 
                     tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons name="home" color={color} size={26} />
+                        // <MaterialCommunityIcons name="home" color={color} size={26} />
+                        <FontAwesome name="home" size={26} color={color} />
                     ),
                 }}
             />
-            <Tab.Screen name='booking' component={BookingScreen} />
-            <Tab.Screen name='profile' component={ProfileScreen} />
+            <Tab.Screen name='Salvos' component={BookingScreen}  options={{
+                    // tabBarLabel:({color}) =>(
+                    //     <Text style={{color: color, fontSize: 12}}>Booking</Text>
+                    // ),
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome name="bookmark" size={26} color={color} />
+                    ),
+                }}
+            />
+            <Tab.Screen name='Perfil' component={ProfileScreen}  options={{
+                  
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome name="user" size={26} color={color} />
+                    ),
+                }}
+             />
         </Tab.Navigator>
     )
 }
