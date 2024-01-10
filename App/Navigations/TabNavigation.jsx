106520f957f490ed68d0b5d8b5365310e26ee5da
Vinicius Lopes <viniloppes2000@gmail.com>
2024-01-09 23:18:51 -0300

    Header Home

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -7,35 +7,44 @@ import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { FontAwesome } from '@expo/vector-icons';
-const Tab = createMaterialBottomTabNavigator();
+import Color from '../Utils/Color'
+const Tab = createBottomTabNavigator();
 export default function TabNavigation() {
     return (
-        <Tab.Navigator  >
+        <Tab.Navigator screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: Color.PRIMARY
+        }} >
             <Tab.Screen name="Inicio" component={HomeScreen}
                 options={{
-                 
+                    tabBarLabel: ({ color }) => (
+                        <Text style={{ color: color, fontSize: 12 }}>Inicio</Text>
+                    ),
                     tabBarIcon: ({ color }) => (
                         // <MaterialCommunityIcons name="home" color={color} size={26} />
+
                         <FontAwesome name="home" size={26} color={color} />
                     ),
                 }}
             />
-            <Tab.Screen name='Salvos' component={BookingScreen}  options={{
-                    // tabBarLabel:({color}) =>(
-                    //     <Text style={{color: color, fontSize: 12}}>Booking</Text>
-                    // ),
-                    tabBarIcon: ({ color }) => (
-                        <FontAwesome name="bookmark" size={26} color={color} />
-                    ),
-                }}
+            <Tab.Screen name='Salvos' component={BookingScreen} options={{
+                tabBarLabel: ({ color }) => (
+                    <Text style={{ color: color, fontSize: 12 }}>Salvos</Text>
+                ),
+                tabBarIcon: ({ color }) => (
+                    <FontAwesome name="bookmark" size={26} color={color} />
+                ),
+            }}
             />
-            <Tab.Screen name='Perfil' component={ProfileScreen}  options={{
-                  
-                    tabBarIcon: ({ color }) => (
-                        <FontAwesome name="user" size={26} color={color} />
-                    ),
-                }}
-             />
+            <Tab.Screen name='Perfil' component={ProfileScreen} options={{
+                tabBarLabel: ({ color }) => (
+                    <Text style={{ color: color, fontSize: 12 }}>Perfil</Text>
+                ),
+                tabBarIcon: ({ color }) => (
+                    <FontAwesome name="user" size={26} color={color} />
+                ),
+            }}
+            />
         </Tab.Navigator>
     )
 }
---
+++ App/Screens/HomeScreen/Header.jsx
@@ -0,0 +1,79 @@
+import { Text, View, Image, StyleSheet, TextInput } from 'react-native'
+import React, { Component } from 'react'
+import { useUser } from '@clerk/clerk-expo'
+import { FontAwesome } from '@expo/vector-icons';
+import Color from '../../Utils/Color'
+
+export default function Header() {
+
+    const { user, isLoading } = useUser();
+
+    return user && (
+        <View style={styles.container}>
+            {/* PROFILE SECTION */}
+            <View style={styles.profileMainContainer}>
+                <View style={styles.profileContainer}>
+                    <Image source={{ uri: user?.imageUrl }} style={styles.userImage} />
+                    <View>
+                        <Text style={{ color: Color.WHITE }}>Welcome!</Text>
+                        <Text style={{ color: Color.WHITE, fontSize: 20 }}>{user?.fullName}!</Text>
+                    </View>
+                </View>
+                <FontAwesome name="bookmark-o" size={27} color="white" />
+            </View>
+            {/* SEARCH BAR SECTION */}
+            <View style={styles.searchBarContainer}>
+                <TextInput placeholder='Pesquisar' style={styles.textInput}/>
+                <FontAwesome name="search" size={24} style={styles.searchButton} color={Color.PRIMARY} />
+            </View>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        paddingTop: 40,
+        backgroundColor: Color.PRIMARY,
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25,
+    },
+    textInput:{
+        padding: 7,
+        paddingHorizontal:  16,
+        backgroundColor: Color.WHITE,
+        width: '85%',
+        borderRadius: 8,
+        fontSize: 16
+    },searchBarContainer:{
+        marginTop: 15,
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10,
+        marginBottom: 10
+    },
+    profileMainContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+
+    },
+    profileContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 10
+    }
+    ,
+    userImage: {
+        width: 45,
+        height: 45,
+        borderRadius: 99
+    },searchButton:{
+        backgroundColor: Color.WHITE,
+        padding: 10,
+        borderRadius: 8
+    }
+})
